e561ee7f572c5d91e09238f21de0c8c8088212a7
cai <caiyax@qq.com>
2019-10-17 23:16:19 +0800

    triangle finish

--- common_class.py
+++ common_class.py
@@ -2,6 +2,7 @@
 # by cyx at 2019/10/17 13:05
 
 import attr
+from typing import *
 
 
 @attr.s
@@ -11,9 +12,48 @@ class Color(object):
 	b = attr.ib(default=0.0)
 	a = attr.ib(default=0.0)
 	maxColor = 255.999999
-
+	
 	def as_rgba_tuple(self):
-		return (int(self.r * self.maxColor), int(self.g * self.maxColor), int(self.b * self.maxColor), int(self.a * self.maxColor),)
+		return (int(self.r * self.maxColor), int(self.g * self.maxColor), int(self.b * self.maxColor),
+		        int(self.a * self.maxColor),)
+
+
+@attr.s
+class Float3D(object):
+	x = attr.ib(default=0.0)
+	y = attr.ib(default=0.0)
+	z = attr.ib(default=0.0)
+	
+	def crossproduct(self, other):
+		"""
+
+		:type other: Float3D
+		"""
+		a1, a2, a3 = self.x, self.y, self.z
+		b1, b2, b3 = other.x, other.y, other.z
+		return Float3D(a2 * b3 - a3 * b2, a3 * b1 - a1 * b3, a1 * b2 - a2 * b1)
+	
+	def normalize(self):
+		s = (self.x * self.x + self.y * self.y + self.z * self.z) ** 0.5;
+		self.x /= s
+		self.y /= s
+		self.z /= s
+		return self
+	
+	def __sub__(self, other):
+		"""
+
+		:type other: Float3D
+		"""
+		return Float3D(self.x - other.x, self.y - other.y, self.z - other.z)
+	
+	def __mul__(self, other):
+		"""
+
+		:type other: Float3D
+		:rtype: float
+		"""
+		return sum((self.x * other.x, self.y * other.y, self.z * other.z))
 
 
 @attr.s
@@ -22,9 +62,19 @@ class Float4D(object):
 	y = attr.ib(default=0.0)
 	z = attr.ib(default=0.0)
 	w = attr.ib(default=0.0)
-
+	
 	def as_int(self):
 		self.x = int(self.x)
 		self.y = int(self.y)
 		self.z = int(self.z)
 		self.w = int(self.w)
+	
+	def __sub__(self, other):
+		"""
+
+		:type other: Float4D
+		"""
+		return Float4D(self.x - other.x, self.y - other.y, self.z - other.z, self.w - other.w)
+	
+	def as_float3d(self) -> Float3D:
+		return Float3D(self.x, self.y, self.z)
--- t1.py
+++ t1.py
@@ -13,18 +13,18 @@ def test1():  # draw wireframe
 	mod = model.ObjParser()
 	mod.open_obj_file('res/obj/african_head.obj')
 	c = common_class.Color(1.0, 1.0, 0, 1.0)
-
+	
 	sx, sy = m.size
-
+	
 	def get_coord(v, size):
 		return int((v + 1.0) / 2 * (size - 0.0001))
-
+	
 	for face in mod.faces:
 		for idx in range(3):
 			p0 = mod.vertexs[face[idx]]
 			p1 = mod.vertexs[face[(idx + 1) % 3]]
 			rasterizer.line(get_coord(p0.x, sx), get_coord(p0.y, sy), get_coord(p1.x, sx), get_coord(p1.y, sy), m, c)
-
+	
 	m = ImageOps.flip(m)
 	m.save('out.png')
 
@@ -33,21 +33,27 @@ def test2():  # draw triangle
 	m = Image.new('RGBA', (500, 500), (0, 0, 0, 255))
 	mod = model.ObjParser()
 	mod.open_obj_file('res/obj/african_head.obj')
-	c = common_class.Color(1.0, 1.0, 0, 1.0)
-
+	
 	sx, sy = m.size
-
+	
 	def get_coord(v, size):
 		return int((v + 1.0) / 2 * (size - 0.0001))
-
+	
+	lightDir = common_class.Float3D(1, -2, 1).normalize()
 	for face in mod.faces:
-		c = common_class.Color(random.random(), random.random(), random.random(), 1.0)
-		for idx in range(3):
-			p0 = mod.vertexs[face[0]]
-			p1 = mod.vertexs[face[1]]
-			p2 = mod.vertexs[face[2]]
-			rasterizer.triangle(get_coord(p0.x, sx), get_coord(p0.y, sy), get_coord(p1.x, sx), get_coord(p1.y, sy), get_coord(p2.x, sx), get_coord(p2.y, sy), m, c)
-
+		p0 = mod.vertexs[face[0]]
+		p1 = mod.vertexs[face[1]]
+		p2 = mod.vertexs[face[2]]
+		n = ((p2 - p0).as_float3d()).crossproduct(
+			((p1 - p0).as_float3d()))
+		n.normalize()
+		intensity = n * lightDir
+		if intensity < 0:
+			continue
+		c = common_class.Color(intensity, intensity, intensity, 1.0)
+		rasterizer.triangle(get_coord(p0.x, sx), get_coord(p0.y, sy), get_coord(p1.x, sx), get_coord(p1.y, sy),
+		                    get_coord(p2.x, sx), get_coord(p2.y, sy), m, c)
+	
 	m = ImageOps.flip(m)
 	m.save('out.png')
 
